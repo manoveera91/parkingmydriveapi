f1bf621abb766f6480d31c35b73e63156de2da6a
Manoj <you@example.com>
2024-06-17 17:59:38 +0530

    make it better

--- database/migrations/2024_05_28_164944_social_id.php
+++ database/migrations/2024_05_28_164944_social_id.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('auth_users', function (Blueprint $table) {
-            $table->string('socialID');
+            $table->string('socialID')->default('no-val');
         });
     }
 
