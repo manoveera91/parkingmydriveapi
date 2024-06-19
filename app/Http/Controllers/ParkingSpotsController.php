7d588ba6ac1fa1750f2f50a34c5cc4fdd4201ee2
Manoj <you@example.com>
2024-06-19 23:12:11 +0530

    make it better

--- app/Http/Controllers/ParkingSpotsController.php
+++ app/Http/Controllers/ParkingSpotsController.php
@@ -149,8 +149,10 @@ class ParkingSpotsController extends Controller
             'auth_owner_id' => 'numeric'
         ]);
 
-        $request['from_date_time'] = $fromDateTime;
-        $request['to_date_time'] = $toDateTime;
+        $request->merge([
+            'from_date_time' => $fromDateTime,
+            'to_date_time' => $toDateTime,
+        ]);
         // Fetch the currently authenticated user using the AuthOwner model
         // $user = Auth::guard('owner')->user();
         $user = AuthOwner::findOrFail($request->input('auth_owner_id'));
